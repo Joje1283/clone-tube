0366f33f4aab578238298ecf161fcb9933db2739
joje1283 <joje1283@gmail.com>
2020-07-06 23:36:28 +0900

    Installed Video.js using npm. Tested video.js in django project.

--- video/views.py
+++ video/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'video/home.html')
+
+
+def video_detail(request, pk):
+    return render(request, 'video/video_detail.html')
+
+
+def test(request):
+    return render(request, 'video/video_detail2.html')
