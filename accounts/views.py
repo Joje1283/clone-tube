14a75358012d8e84faac3036e93ec39af41f508a
joje1283 <joje1283@gmail.com>
2020-07-13 23:01:21 +0900

    Added login page(bootstrap).

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        pw = request.POST.get('password')
+
+    return render(request, 'accounts/login.html')
