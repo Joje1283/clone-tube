4437518fe370f90c2b480372809ba5250970f57f
joje1283 <joje1283@gmail.com>
2020-07-16 23:19:25 +0900

    Created video table. Completed test the video upload.

--- video/models.py
+++ video/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+def user_directory_path(instance, filename):
+    return 'user_{}/{}'.format(instance.user, filename)
+
+
+class Video(models.Model):
+    title = models.CharField(verbose_name='title', max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    description = models.TextField(verbose_name='description')
+    upload = models.FileField(upload_to=user_directory_path)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
