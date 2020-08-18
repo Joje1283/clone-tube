85dbd457d1fb804b9d5dc612f996a610077cf690
joje1283 <joje1283@gmail.com>
2020-08-18 23:59:36 +0900

    subscribe 구현 진행 중

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, models
 from imagekit.processors import Thumbnail
 from imagekit.models import ProcessedImageField
+import json
 
 
 def user_profile_thumbnail_directory_path(instance, filename):
@@ -15,3 +16,39 @@ class User(AbstractUser):
         options={'quality': 60},
         null=True
     )  # 저장 옵션
+    subscribing = models.CharField(max_length=5000, default='[]')
+    subscribing_by = models.CharField(max_length=5000, default='[]')
+
+    def subscribe(self, user):
+        subscribing = json.loads(self.subscribing)
+        if not user.pk in subscribing and not self.pk in user.subscribing_by:
+            user.subscribed(self)
+            subscribing = json.loads(self.subscribing)
+            subscribing.append(user.pk)
+            self.subscribing = json.dumps(subscribing)
+            self.save()
+
+    def subscribed(self, user):
+        if not user.pk in self.subscribing_by:
+            subscribing_by = json.loads(self.subscribing_by)
+            subscribing_by.append(user.pk)
+            self.subscribing_by = json.dumps(subscribing_by)
+            self.save()
+
+    def unsubscribe(self, user):
+        subscribing = json.loads(self.subscribing)
+        if user.pk in subscribing and self.pk in user.subscribing_by:
+            user.unsubscribed(self)
+
+            if user.pk in subscribing:
+                subscribing.remove(user.pk)
+            self.subscribing = json.dumps(subscribing)
+            self.save()
+
+    def unsubscribed(self, user):
+        subscribing_by = json.loads(self.subscribing_by)
+        if user.pk in subscribing_by:
+            if user.pk in subscribing_by:
+                subscribing_by.remove(user.pk)
+            self.subscribing_by = json.dumps(subscribing_by)
+            self.save()
